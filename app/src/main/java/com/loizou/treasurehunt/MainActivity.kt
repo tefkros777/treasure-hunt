a78097c053afcf65df207928b69b1ce7e1a560b2
tefkros <constantinoslo98@gmail.com>
2020-11-28 19:00:56 +0000

    Add game mode selection screen

--- app/src/main/java/com/loizou/treasurehunt/MainActivity.kt
+++ app/src/main/java/com/loizou/treasurehunt/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.service.autofill.UserData
 import android.util.Log
+import android.view.View
 import android.view.ViewGroup
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +16,17 @@ class MainActivity : AppCompatActivity() {
         val rootLayout = findViewById<ViewGroup>(R.id.rootLayout)
         // Greet user
         showMessage(rootLayout, "Welcome to treasure hunt!")
+
+        val btnHuntMode = findViewById<View>(R.id.btnHuntMode);
+        val btnBurialMode = findViewById<View>(R.id.btnBurialMode);
+
+        btnHuntMode.setOnClickListener {
+            showMessage(rootLayout, "TREASURE HUNT MODE")
+        }
+
+        btnBurialMode.setOnClickListener {
+            showMessage(rootLayout, "TREASURE BURIAL MODE")
+        }
 
     }
 }
