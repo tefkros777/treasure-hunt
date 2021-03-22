a1342b593d981a49b90265e0b333adc49a402824
tefkros <constantinoslo98@gmail.com>
2021-03-22 13:24:54 +0000

    Add firebase authentication

--- app/src/main/java/com/loizou/treasurehunt/GameModeSelectionActivity.kt
+++ app/src/main/java/com/loizou/treasurehunt/GameModeSelectionActivity.kt
@@ -6,19 +6,28 @@ import android.os.Bundle
 import android.util.Log
 import android.view.ViewGroup
 import androidx.cardview.widget.CardView
+import com.google.android.material.textview.MaterialTextView
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class GameModeSelectionActivity : AppCompatActivity() {
+    private val mCurrentUser: FirebaseUser = Firebase.auth.currentUser!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_mode_selection)
         Log.v(LOG_TAG, "Main Activity Loaded")
 
         val rootLayout = findViewById<ViewGroup>(R.id.rootLayout)
+        val btnHuntMode = findViewById<CardView>(R.id.btnHuntMode)
+        val btnBurialMode = findViewById<CardView>(R.id.btnBurialMode)
+        val tvAhoy = findViewById<MaterialTextView>(R.id.tvAhoy)
+        tvAhoy.append(" ${mCurrentUser.displayName}!")
+
         // Greet user
         showMessage(rootLayout, "Welcome to treasure hunt!")
 
-        val btnHuntMode = findViewById<CardView>(R.id.btnHuntMode);
-        val btnBurialMode = findViewById<CardView>(R.id.btnBurialMode);
 
         btnHuntMode.setOnClickListener {
             showMessage(rootLayout, "TREASURE HUNT MODE")
--- app/src/main/java/com/loizou/treasurehunt/LoginActivity.kt
+++ app/src/main/java/com/loizou/treasurehunt/LoginActivity.kt
@@ -2,30 +2,50 @@ package com.loizou.treasurehunt
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.BoringLayout.make
 import android.util.Log
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.snackbar.Snackbar.make
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textview.MaterialTextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         Log.v(LOG_TAG, "Login Activity Loaded")
 
+        // Initialise Firebase auth
+        mAuth = Firebase.auth
+
+        val etEmail = findViewById<TextInputEditText>(R.id.etEmail)
+        val etPass = findViewById<TextInputEditText>(R.id.etPassword)
+
         // Login button
         val btnLogin = findViewById<MaterialButton>(R.id.btnLogin)
         btnLogin.setOnClickListener {
-            closeKeyBoard()
+//            TODO: UNCOMMENT SECTION - DEBUGGING ONLY
+//            if (!etEmail.text.isNullOrBlank() || !etPass.text.isNullOrBlank()) {
+                closeKeyBoard()
 
-            // TODO: IMPLEMENT USER LOGIN WITH FIREBASE
+                val email = etEmail.text!!.trim().toString()
+                val pass = etPass.text!!.trim().toString()
 
-            val intent = Intent(this, GameModeSelectionActivity::class.java)
-            startActivity(intent)
-            finish()
+                signIn(email, pass)
+//            } else {
+//                debugLog("Could not sign in - Email or pass were null")
+//            }
         }
-
     }
 
     private fun closeKeyBoard() {
@@ -34,6 +54,26 @@ class LoginActivity : AppCompatActivity() {
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(view.windowToken, 0)
         }
+    }
+
+    private fun signIn(email: String, password: String) {
+        // TODO: DEBUGGING ONLY
+        val email = "test@test.com"
+        val password = "123456"
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    debugLog("Firebase login: Successful")
+
+                    val intent = Intent(this, GameModeSelectionActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    debugLog("Firebase login: Unsuccessful")
+                    debugLog(task.exception.toString())
+                }
+            }
     }
 
 }
