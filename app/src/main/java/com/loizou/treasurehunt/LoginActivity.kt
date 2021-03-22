945467b18002fc1cc93dc46dedb318c0c74fd0cb
tefkros <constantinoslo98@gmail.com>
2021-03-22 13:30:12 +0000

    Refactoring

--- app/src/main/java/com/loizou/treasurehunt/LoginActivity.kt
+++ app/src/main/java/com/loizou/treasurehunt/LoginActivity.kt
@@ -3,15 +3,11 @@ package com.loizou.treasurehunt
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.text.BoringLayout.make
 import android.util.Log
+import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.button.MaterialButton
-import com.google.android.material.snackbar.Snackbar.make
 import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textview.MaterialTextView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -28,24 +24,24 @@ class LoginActivity : AppCompatActivity() {
         // Initialise Firebase auth
         mAuth = Firebase.auth
 
+    }
+
+    fun login(view: View) {
         val etEmail = findViewById<TextInputEditText>(R.id.etEmail)
         val etPass = findViewById<TextInputEditText>(R.id.etPassword)
-
-        // Login button
-        val btnLogin = findViewById<MaterialButton>(R.id.btnLogin)
-        btnLogin.setOnClickListener {
 //            TODO: UNCOMMENT SECTION - DEBUGGING ONLY
 //            if (!etEmail.text.isNullOrBlank() || !etPass.text.isNullOrBlank()) {
-                closeKeyBoard()
-
-                val email = etEmail.text!!.trim().toString()
-                val pass = etPass.text!!.trim().toString()
-
-                signIn(email, pass)
+        closeKeyBoard()
+        val email = etEmail.text!!.trim().toString()
+        val pass = etPass.text!!.trim().toString()
+        firebaseSignIn(email, pass)
 //            } else {
 //                debugLog("Could not sign in - Email or pass were null")
 //            }
-        }
+    }
+
+    fun createNewUser(view: View) {
+        
     }
 
     private fun closeKeyBoard() {
@@ -56,7 +52,7 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    private fun signIn(email: String, password: String) {
+    private fun firebaseSignIn(email: String, password: String) {
         // TODO: DEBUGGING ONLY
         val email = "test@test.com"
         val password = "123456"
@@ -64,7 +60,6 @@ class LoginActivity : AppCompatActivity() {
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     debugLog("Firebase login: Successful")
-
                     val intent = Intent(this, GameModeSelectionActivity::class.java)
                     startActivity(intent)
                     finish()
