e94493acf53e1e611917af06996f8872abdcd783
tefkros <constantinoslo98@gmail.com>
2021-04-05 19:51:10 +0100

    Add interface to add new waypoint to treasure burial

--- app/src/main/java/com/loizou/treasurehunt/AddWaypointActivity.kt
+++ app/src/main/java/com/loizou/treasurehunt/AddWaypointActivity.kt
@@ -2,10 +2,18 @@ package com.loizou.treasurehunt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.textview.MaterialTextView
 
 class AddWaypointActivity : AppCompatActivity() {
+
+    private var WAYPOINT_NUM = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_waypoint)
+
+        title = getString(R.string.waypoint_num, ++WAYPOINT_NUM)
+
+
     }
 }
