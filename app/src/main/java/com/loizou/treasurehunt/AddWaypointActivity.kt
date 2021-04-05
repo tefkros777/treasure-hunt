5d30fa4806ac5974962d4af244b8802afefece74
tefkros <constantinoslo98@gmail.com>
2021-04-05 22:33:33 +0100

    Add map in AddWaypointActivity

--- app/src/main/java/com/loizou/treasurehunt/AddWaypointActivity.kt
+++ app/src/main/java/com/loizou/treasurehunt/AddWaypointActivity.kt
@@ -2,11 +2,14 @@ package com.loizou.treasurehunt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.material.textview.MaterialTextView
 
-class AddWaypointActivity : AppCompatActivity() {
+class AddWaypointActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private var WAYPOINT_NUM = 0
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +18,10 @@ class AddWaypointActivity : AppCompatActivity() {
         title = getString(R.string.waypoint_num, ++WAYPOINT_NUM)
 
 
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        // do stuff ...
     }
 }
