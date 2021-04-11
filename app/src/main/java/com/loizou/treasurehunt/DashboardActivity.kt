60ee96a93d36999ffae268571764b8666e3672b6
tefkros <constantinoslo98@gmail.com>
2021-04-11 16:31:02 +0100

    Add logout functionality

--- app/src/main/java/com/loizou/treasurehunt/DashboardActivity.kt
+++ app/src/main/java/com/loizou/treasurehunt/DashboardActivity.kt
@@ -3,12 +3,15 @@ package com.loizou.treasurehunt
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AlertDialog
 import com.google.android.material.textview.MaterialTextView
+import com.google.firebase.auth.FirebaseAuth
 import com.loizou.treasurehunt.Data.UserSingleton
 
 class DashboardActivity : AppCompatActivity() {
+    private var mAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,8 +30,6 @@ class DashboardActivity : AppCompatActivity() {
             .apply { text = UserSingleton.activeUser.displayName }
         val tvEmail = findViewById<MaterialTextView>(R.id.tvEmail)
             .apply { text = UserSingleton.activeUser.email }
-        val tvScore = findViewById<MaterialTextView>(R.id.tvScore)
-            .apply { text = UserSingleton.activeUser.score.toString() }
     }
 
     fun huntMode(v: View) {
@@ -48,6 +49,29 @@ class DashboardActivity : AppCompatActivity() {
             .setCancelable(false)
             .setPositiveButton("Yes") { dialog, id ->
                 // TODO: Maybe perform some saving?
+                finish()
+            }
+            .setNegativeButton("No") { dialog, id ->
+                dialog.cancel()
+            }
+        builder.create().show()
+    }
+
+    fun logout(view: View) {
+        // Show confirmation dialog
+        val builder = AlertDialog.Builder(this)
+            .setTitle(R.string.app_name)
+            .setMessage(R.string.signout_promt)
+            .setCancelable(true)
+            .setPositiveButton("Yes") { dialog, id ->
+                // TODO: Maybe perform some saving?
+                debugLog("Logging out")
+                mAuth.signOut()
+
+                // Go back to login screen
+                val intent = Intent(this, LoginActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+                startActivity(intent)
                 finish()
             }
             .setNegativeButton("No") { dialog, id ->
